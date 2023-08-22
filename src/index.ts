5e31c9207933f3ead01ef4809888137167be9733
Nitin Mittal <nitinmittal44066@gmail.com>
2023-08-22 17:25:16 +0400

    feat: new updates

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ const test = () => {
     console.log("this is a test repo")
     console.log("update in index")
     console.log("new update in index")
+    console.log("2nd new update in index")
 }
 
 test();
